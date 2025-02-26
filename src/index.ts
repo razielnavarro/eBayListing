e2a57f4f9d530e440c06870f8f1d459731b01d3a
razielnavarro <elrazand@gmail.com>
2025-02-26 12:04:17 -0500

    feat: add server setup to index.ts for Hono application

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+
 import { listingInfo } from "./controllers/listingInfo.controller";
 
 const app = new Hono();
 
 app.route('listingInfo', listingInfo);
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
 
 export default app;
