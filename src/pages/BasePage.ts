48242c8ac979ea1bc7af01795521cb87f04bd897
razielnavarro <elrazand@gmail.com>
2025-03-05 14:42:24 -0500

    fix: update navigation wait strategy to use 'networkidle0' for improved reliability

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -140,7 +140,7 @@ export default class BasePage {
       { hidden: true }
     );
     await this.page.waitForNavigation({
-      waitUntil: "networkidle2",
+      waitUntil: 'networkidle0',
       timeout: 15000,
     });
     await this.page.content();
