f29251f5c4bec877c7d15ea2bfb4745ff81d1a0d
razielnavarro <elrazand@gmail.com>
2025-03-05 13:56:53 -0500

    feat: update getShipping method to improve currency handling and return type

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -57,33 +57,23 @@ export default class BasePage {
 
   // Get shipping
   // costs
-  async getShipping() {
-    let shippingCost: string;
-    // shipping with $ Amount
+  async getShipping(): Promise<string> {
     try {
-      shippingCost = await this.page.$eval(
-        "#mainContent > div.vim.d-vi-evo-region > div.vim.d-shipping-minview.mar-t-20 > div > div > div > div:nth-child(1) > div > div > div > div.ux-labels-values__values.col-9 > div > div:nth-child(1) > span:nth-child(1)",
-        (el) => el.innerHTML
+      let shippingCost = await this.page.$eval(
+        "div.ux-layout-section--shipping div.ux-labels-values--shipping span.ux-textspans.ux-textspans--BOLD",
+        (el) => el.textContent?.trim() || ""
       );
       // if shipping shows in a different currency, show amount in USD
       if (!shippingCost.includes("$")) {
         shippingCost = await this.page.$eval(
-          "#mainContent > div > div.vim.d-shipping-minview.mar-t-20 > div > div > div > div:nth-child(1) > div > div > div > div.ux-labels-values__values.col-9 > div > div:nth-child(1) > span.ux-textspans.ux-textspans--SECONDARY.ux-textspans--BOLD",
+          "div.ux-layout-section--shipping div.ux-labels-values--shipping span.ux-textspans.ux-textspans--SECONDARY.ux-textspans--BOLD",
           (el) => el.innerHTML
         );
       }
+      return shippingCost;
     } catch (error) {
-      // shipping with Free Shipping
-      try {
-        shippingCost = await this.page.$eval(
-          "#mainContent > div.vim.d-vi-evo-region > div.vim.d-shipping-minview.mar-t-20 > div > div > div > div:nth-child(1) > div > div > div.ux-labels-values__values.col-9 > div > div > span.ux-textspans.ux-textspans--BOLD",
-          (el) => el.innerHTML
-        );
-      } catch (error) {
-        throw new Error(`Could not get price: ${error}`);
-      }
+      throw new Error(`Could not get shipping cost: ${error}`);
     }
-    return shippingCost;
   }
 
   // Choose USA as the
