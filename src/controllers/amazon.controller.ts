3f4938289f8048b187f359dbd9842dd27d396c1f
razielnavarro <elrazand@gmail.com>
2025-03-06 17:53:24 -0500

    feat: implement Amazon scraping controller and page initial layout class for handling URL requests

---
+++ src/controllers/amazon.controller.ts
@@ -0,0 +1,20 @@
+import { Hono } from "hono";
+import * as puppeteer from "puppeteer";
+import AmazonPage from "../pages/amazonScraping";
+
+export const amazonScraping = new Hono();
+
+amazonScraping.post("/", async (c) => {
+  const url = await c.req.json();
+  if (!url) {
+    return c.json({ error: "URL is required" }, 400);
+  }
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const amazonPage = new AmazonPage(browser, page);
+
+  await amazonPage.visit(url);
+
+  await browser.close();
+
+});
---
+++ src/pages/amazonScraping.ts
@@ -0,0 +1,23 @@
+import * as puppeteer from "puppeteer";
+
+export default class AmazonPage {
+  private browser: puppeteer.Browser;
+  private page: puppeteer.Page;
+
+  constructor(browser: puppeteer.Browser, page: puppeteer.Page) {
+    this.browser = browser;
+    this.page = page;
+  }
+
+  // Helper function to parse the shipping cost string
+  private parseDollarValue(text: string): string {
+    // This regex finds a dollar sign followed by the number with optional decimals.
+    const regex = /\$\s*\d+(?:[.,]\d+)?/;
+    const match = text.match(regex);
+    return match ? match[0].replace(/\s+/g, "") : text;
+  }
+
+  async visit(url: string) {
+    await this.page.goto(url);
+  }
+}
